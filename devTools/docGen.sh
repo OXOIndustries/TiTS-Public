2986c44037b33d374dd036fd350605e37dc7444f
Fake Name <cat176@gmail.com>
2014-01-11 02:32:49 -0800

    Updating tooling scripts, submodule.

---
+++ devTools/docGen.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+naturaldocs -i ./classes \
+            -o FramedHTML ../webDocs/ \
+            -p ../natDocsTmp
--- devTools/wikiManage.sh
+++ devTools/wikiManage.sh
@@ -2,16 +2,24 @@
 
 STAGE="$1"
 
-DIRECTORY=../wiki
+WIKIDIR=../wiki
+WEGDIR=../webDocs
 
 if [ "$STAGE" = "setup" ]; then
 	echo "Setup stage for wiki"
 
-	if [ ! -d "$DIRECTORY" ]; then
+	if [ ! -d "$WIKIDIR" ]; then
 		git clone "git@github.com:Fenoxo/sourceTiTS.wiki" ../wiki
 	else
 		(cd ../wiki && git pull)
 		(cd ../wiki && git reset --hard HEAD)
+	fi 
+	
+	if [ ! -d "$WEGDIR" ]; then
+		git clone -b gh-pages "git@github.com:Fenoxo/sourceTiTS" ../webDocs
+	else
+		(cd ../webDocs && git pull)
+		(cd ../webDocs && git reset --hard HEAD)
 
 	fi
 
@@ -20,8 +28,16 @@ fi
 if [ "$STAGE" = "commit" ]; then
 	echo "Commit stage for wiki"
 	(cd ../wiki && git add -u)
+	echo "Added new files"
 	(cd ../wiki && git commit -m "Automated build commited by BuildBot CoC/TiTS-Builder")
 	(cd ../wiki && git push)
+
+	echo "Commit stage for Pages"
+	(cd ../webDocs && git add -u)
+	(cd ../webDocs && git add *)
+	echo "Added new files"
+	(cd ../webDocs && git commit -m "Automated build commited by BuildBot CoC/TiTS-Builder")
+	(cd ../webDocs && git push)
 
 fi
 
