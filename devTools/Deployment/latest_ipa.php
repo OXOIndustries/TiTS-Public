5d931dab84ad8dea681055cb80d017ef13000d30
Gedan <gedan@ganked.me>
2016-03-05 17:36:15 +0000

    Updated the cache control stuff on other scripts with some more science

--- devTools/Deployment/latest_ipa.php
+++ devTools/Deployment/latest_ipa.php
@@ -18,19 +18,19 @@ if (is_file($latest_filename))
 	if (isset($_SERVER["HTTP_IF_MODIFIED_SINCE"]) && strtotime($_SERVER["HTTP_IF_MODIFIED_SINCE"]) >= filemtime($latest_filename))
 	{
 		header($_SERVER["SERVER_PROTOCOL"] . " 304 Not Modified");
-		header("Cache-Control: max-age=12096000, public", true);
-		header("Expires: " . gmdate("D, d M Y H:i:s \G\M\T", time() + (60 * 60)), true);
+		header("Cache-Control: public", true);
+		header("Last-Modified: " . gmdate("D, d M Y H:i:s \G\M\T", filemtime($latest_filename)), true);
 		header("Pragma: cache", true);
 		exit;
 	}
 	else
 	{
 		header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
-		header("Cache-Control: max-age=12096000, public", true);
-		header("Expires: " . gmdate("D, d M Y H:i:s \G\M\T", time() + (60 * 60)), true);
+		header("Cache-Control: public", true);
 		header("Content-Type: application/octet-stream", true);
 		header("Accept-Ranges: bytes", true);
 		header("Content-Length: " . filesize($latest_filename), true);
+		header("Last-Modified: " . gmdate("D, d M Y H:i:s \G\M\T", filemtime($latest_filename)), true);
 		header("Connection: keep-alive", true);
 		header("Content-Disposition: attachment; filename=\"$latest_filename\"");
 		readfile($latest_filename);
